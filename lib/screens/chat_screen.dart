ccce26ef0523f1601bcf2a86f225f95ac70cdb4e
sachin <sachinbansal091@gmail.com>
2024-06-14 14:37:19 +0530

    cahnges has been made

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,33 +1,53 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_chat_app/widgets/chat_messages.dart';
 import 'package:flutter_chat_app/widgets/new_messages.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    // final token = await fcm.getToken();
+    // print('tioke is:$token');
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter chat app'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
-              ))
-        ],
-      ),
-      body:const Column(
-        children: [
-           Expanded(child: ChatMessages()),  
-          NewMessage(), 
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: const Text('Flutter chat app'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: Icon(
+                  Icons.exit_to_app,
+                  color: Theme.of(context).colorScheme.primary,
+                ))
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(child: ChatMessages()),
+            NewMessage(),
+          ],
+        ));
   }
 }
